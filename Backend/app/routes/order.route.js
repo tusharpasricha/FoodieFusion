30fd7e6f5e8af6d6e677921bcb56282e9e0a5d03
Tushar Pasricha <tusharpasricha21@gmail.com>
2024-09-01 10:32:26 +0530

    added auth to orders

--- Backend/app/routes/order.route.js
+++ Backend/app/routes/order.route.js
@@ -1,11 +1,13 @@
 module.exports = (app) => {
   const orders = require("../controllers/order.controller.js");
+  const authJwt = require("../middleware/middleware.js");
+
 
   var router = require("express").Router();
 
-  router.post("/", orders.create);
+  router.post("/", authJwt.verifyToken, orders.create);
 
-  router.get("/", orders.findAll);
+  router.get("/", authJwt.verifyToken, orders.findAll);
 
   app.use("/api/orders", router);
 };
